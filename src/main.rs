8fdfc73c01f8781e92555d08520cfa600e94373f
Beatriz <bia.lopes17@gmail.com>
2023-01-05 10:07:09 -0300

    Correções Projeto 1

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,33 @@
-fn start() {
-    println!("👏 Seja bem vindo(a)! 👏\n");
-    println!("Você deseja adicionar um novo TODO? (s/n)");
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
 }
 
 fn main() {
-    start();
-
-    fn input() -> String {
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).unwrap();
-        buf.trim().to_string()
-     }
-        if let mut answer = 's' {
-            println!("Qual o nome do arquivo TODO você deseja criar? \n");
-            println!("Você digitou XXXXXXXXXX. Confirme o TODO a ser criado.\n");
-            println!("O arquivo TODO foi criado com sucesso.\n");
-            return start();
+    println!("👏 Seja bem vindo(a)! 👏 \n");
+    loop {
+        if start() {
+            break;
         }
-        else if let answer = 'n' {
-                println!("Finalizando o programa.");
-            }
-            else {
-                println!("Insira uma entrada valida. Se deseja criar um novo TODO, insira 's', senão, insira 'n'.");
-
-            }
-
-        
-
+    }
 }
 
+fn start() -> bool {
+    println!("Você deseja adicionar um novo TODO? (s/n)");
 
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
+    let answer = input();
+    if answer == "s" {
+        println!("Qual o nome do arquivo TODO você deseja criar?");
+        let name: String = input();
+        println!("Seu novo arquivo TODO foi criado com sucesso.");
+        println!("{}", name);
+        false
+    } else if answer == "n" {
+        println!("Finalizando o programa.");
+        true
+    } else {
+        println!("Insira uma entrada valida. Se deseja criar um novo TODO, insira 's', senão, insira 'n'.");
+        false
+    }
+}
--- target/debug/todo.d
+++
@@ -1,1 +0,0 @@
-/home/beatriz/rustlings/todo/target/debug/todo: /home/beatriz/rustlings/todo/src/main.rs
