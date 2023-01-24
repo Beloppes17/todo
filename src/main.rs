00878f788cb65215e2692270fe765bd1ceee9870
Beatriz <bia.lopes17@gmail.com>
2023-01-24 11:17:20 -0300

    Projeto 2: Adicionando struct ao fluxo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-fn input() -> String {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).unwrap();
-    buf.trim().to_string()
-}
+use std::io::{Stdin, Stdout, Write};
 
 fn main() {
     println!("👏 Seja bem vindo(a)! 👏 \n");
@@ -13,9 +9,38 @@ fn main() {
     }
 }
 
+#[derive(Debug, Clone)]
+struct Todo {
+    message: String,
+}
+
+impl Todo {
+    pub fn new(message: String) -> Self {
+        Todo { message }
+    }
+}
+struct Terminal {
+    stdin: Stdin,
+    stdout: Stdout,
+}
+
+impl Terminal {
+    fn new() -> Terminal {
+        Terminal {
+            stdin: (std::io::stdin()),
+            stdout: (std::io::stdout()),
+        }
+    }
+}
+
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+}
+
 fn start() -> bool {
     println!("Você deseja adicionar um novo TODO? (s/n)");
-
     let answer = input();
     if answer == "s" {
         println!("Qual o nome do arquivo TODO você deseja criar?");
